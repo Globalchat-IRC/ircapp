5676017cbcd669aca33c640be8ff54c470b69874
Fran <fnaveira@MacBook-Air-de-Fran.local>
2026-01-31 01:57:23 +0100

    Eliminados headers CORS duplicados del PHP - Apache ya los maneja

--- gateway/mixcloud_stream_extractor.php
+++ gateway/mixcloud_stream_extractor.php
@@ -5,12 +5,10 @@
  */
 
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
+// Los headers CORS los maneja Apache, no los duplicamos aquí
 
 // Manejar preflight OPTIONS
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit;
 }
